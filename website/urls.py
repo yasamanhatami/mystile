5f27f07b10707825d4b71e872a1dc1ba3d0a4a80
yasamanhatami <152697252+yasamanhatami@users.noreply.github.com>
2024-03-13 10:55:40 +0330

    created render html files
    added templates folder html files to website app and render to views

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from website.views import *
 urlpatterns = [
-    path('home',index_views),
+    path('',index_views),
     path('about',about_views),
     path('contact',contact_views)
 
--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_views (request):
-    return HttpResponse('<h1>home page</h1>')
+    return render(request,'website/index.html')
 def about_views (request):
-    return HttpResponse('<h1>about page</h1>')
+    return render(request,'website/about.html')
 def contact_views (request):
-    return HttpResponse('<h1>contact page</h1>')
+    return render(request,'website/contact.html')
 
 # Create your views here.
