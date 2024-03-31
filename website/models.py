5df41e0e53d71b923eb67de23f98235eab9de89d
yasamanhatami <152697252+yasamanhatami@users.noreply.github.com>
2024-03-31 08:55:39 +0330

    Database creation
    I created the blog post and contact database and wrote the required fields

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cantact(models.Model):
+    name=models.CharField(max_length=225)
+    email=models.EmailField()
+    subject=models.CharField(max_length=225)
+    message=models.TextField()
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
+
+    
