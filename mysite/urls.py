43fe5696178c2d5ca62226483e23677fa3dd6c0a
yasamanhatami <yasamanhatami13871@gmail.com>
2024-06-22 17:32:43 +0330

    add next,prev image url and Add / to the blog address in the main site address

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('url address','view')
     path('',include('website.urls')),
-    path('blog',include('blog.urls'))
+    path('blog/',include('blog.urls'))
 
 ]
 urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
