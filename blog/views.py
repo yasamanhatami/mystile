e9c42f548cfdfb7f17b4e7ce53fd657ed6e6a521
yasamanhatami <yasamanhatami13871@gmail.com>
2024-06-22 15:44:44 +0330

    add prev and next query to views.py

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,8 @@ def blog_single(request,pid):
     post=get_object_or_404(Post,id=pid,status=1,published_date__lte=timezone.now())
     post.counted_views=post.counted_views+1
     post.save()
-    next_post = Post.objects.filter(published_date__lte=timezone.now(),status=1)[post.id]
-    previous_post = Post.objects.filter(published_date__lte=timezone.now(),status=1)[post.id-2]
+    next_post = Post.objects.filter(id__gt=pid,published_date__lte=timezone.now(),status=1).first()
+    previous_post = Post.objects.filter(id__lt=pid,published_date__lte=timezone.now(),status=1).last()
     context={'post':post,'next_post':next_post,'previous_post':previous_post}
     return render(request,'blog/blog-single.html',context)
 #def test(request,pid):
