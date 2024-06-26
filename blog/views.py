4a9b5359e83b07fafec23a686b2a9238bf2d59ad
yasamanhatami <yasamanhatami13871@gmail.com>
2024-06-26 17:39:30 +0330

    Filter posts based on search form

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
+    if kwargs.get('cat_name')!=None:
+        posts=Post.objects.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username')!=None:
+        posts=Post.objects.filter(author__username=kwargs['author_username'])
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request,pid):
@@ -14,7 +18,14 @@ def blog_single(request,pid):
     previous_post = Post.objects.filter(id__lt=pid,published_date__lte=timezone.now(),status=1).last()
     context={'post':post,'next_post':next_post,'previous_post':previous_post}
     return render(request,'blog/blog-single.html',context)
-def test(request):
+#def test(request):
     #posts=get_object_or_404(Post,id=pid)
     #context={'posts':posts}
-    return render(request,'test.html')
+    #return render(request,'test.html')
+def blog_search(request):
+    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
+    if request.method == "GET":
+        if s:=request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
