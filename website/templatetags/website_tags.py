69d9d21f5ca89c21df0fe41db75f39d31d4a2ee7
yasamanhatami <yasamanhatami13871@gmail.com>
2024-06-27 18:33:58 +0330

    use inclusion_tag

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -2,7 +2,7 @@ from django import template
 from blog.models import Post
 from django.utils import timezone
 register = template.Library()
-@register.simple_tag(name='blogposts')
-def function():
-    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
-    return posts
+@register.inclusion_tag('website/website-latest-post.html')
+def blogposts(arg=6):
+    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)[:arg]
+    return {'posts':posts}
